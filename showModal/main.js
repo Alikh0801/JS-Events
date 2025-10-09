554b654535f6622390a812b71da849d72d6f1a07
kheirkheberli <ali.xov08@gmail.com>
2025-10-09 12:00:07 +0400

    update

---
+++ showModal/main.js
@@ -0,0 +1,19 @@
+const quickMode = document.querySelector('.quick-mode');
+const eyeBtn = document.querySelector('.lucide-eye-icon');
+const closeBtn = document.getElementById('close');
+
+eyeBtn.addEventListener('click', () => {
+
+    quickMode.style.display = 'flex';
+});
+
+closeBtn.addEventListener('click', () => {
+    quickMode.style.display = 'none';
+});
+
+quickMode.addEventListener('click', (e) => {
+    if (e.target === quickMode) {
+        quickMode.style.display = 'none';
+        document.body.style.overflow = '';
+    }
+});
